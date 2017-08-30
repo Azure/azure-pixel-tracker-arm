9522773dd7ef9b92cf6f1826785adf998d95bc6f
Daniel Ciborowski <dciborow@microsoft.com>
2017-08-30 06:04:00 +0000

    Merged PR 57130: Updated deploySpring.ps1
    Updated deploySpring.ps1
    
    Related work items: #80039

--- deploySpring.ps1
+++ deploySpring.ps1
@@ -2,6 +2,9 @@ param(
     [string]
     $appdirectory,
 
+    [string],
+    $file
+
     [string]
     $resourceGroup,
 
@@ -23,8 +26,7 @@ $url = $xml.SelectNodes("//publishProfile[@publishMethod=`"FTP`"]/@publishUrl").
 Set-Location $appdirectory
 $webclient = New-Object -TypeName System.Net.WebClient
 $webclient.Credentials = New-Object System.Net.NetworkCredential($username,$password)
-$relativepath = (Resolve-Path -Path $appdirectory -Relative).Replace(".\", "").Replace('\', '/')
-$uri = New-Object System.Uri("$url/$relativepath")
+$uri = New-Object System.Uri("$url/$file")
 "Uploading to " + $uri.AbsoluteUri
 $webclient.UploadFile($uri, $file.FullName)
 $webclient.Dispose()
