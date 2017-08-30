d5fb8859501256b02fe98baa4dadfcd8d731371c
Daniel Ciborowski <dciborow@microsoft.com>
2017-08-30 04:46:23 +0000

    Merged PR 57106: Merge psScript to master
    Related work items: #80039

---
+++ deploySpring.ps1
@@ -0,0 +1,23 @@
+# Get publishing profile for the web app
+$xml = (Get-AzureRmWebAppPublishingProfile -Name $webappname `
+-ResourceGroupName $resourceGroup `
+-OutputFile null)
+
+# Extract connection information from publishing profile
+$username = $xml.SelectNodes("//publishProfile[@publishMethod=`"FTP`"]/@userName").value
+$password = $xml.SelectNodes("//publishProfile[@publishMethod=`"FTP`"]/@userPWD").value
+$url = $xml.SelectNodes("//publishProfile[@publishMethod=`"FTP`"]/@publishUrl").value
+
+# Upload files recursively 
+Set-Location $appdirectory
+$webclient = New-Object -TypeName System.Net.WebClient
+$webclient.Credentials = New-Object System.Net.NetworkCredential($username,$password)
+$files = Get-ChildItem -Path $appdirectory -Recurse | Where-Object{!($_.PSIsContainer)}
+foreach ($file in $files)
+{
+    $relativepath = (Resolve-Path -Path $file.FullName -Relative).Replace(".\", "").Replace('\', '/')
+    $uri = New-Object System.Uri("$url/$relativepath")
+    "Uploading to " + $uri.AbsoluteUri
+    $webclient.UploadFile($uri, $file.FullName)
+} 
+$webclient.Dispose()
